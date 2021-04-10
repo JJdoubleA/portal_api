bbe84c7212a8a8b2df29ea83612a96745d15edab
JJdoubleA <jaytdot2k@gmail.com>
2021-04-09 22:48:54 -0400

    api2

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly RestAPIContext _context;
+
+        public CustomersController(RestAPIContext context)
+        {
+            _context = context;
+        }        
+
+        //----------------------------------- Retrieving information from all Customers -----------------------------------\\
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.customers.ToListAsync();
+        }
+        
+        //-----------------------------------  -----------------------------------\\
+
+        // GET: api/Customer/id
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<Customer>> GetCustomer(long id)
+        // {
+        //     var customers = await _context.customers.FindAsync(id);
+
+        //     if (customers == null)
+        //     {
+        //         return NotFound();
+        //     }
+
+        //     return customers;
+        // }
+        
+        //----------------------------------- Matching Users portal-email with Mysql-email -----------------------------------\\
+
+        // GET: api/Customers/1/email
+        [HttpGet("{email}")]
+        public async Task<ActionResult<bool>> GetCustomer(string email)
+        {
+           var customers = await _context.customers.Where(customer => customer.email_of_company_contact == email).ToListAsync();
+            if(!CustomersExists(email))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // We will add the endpoint above that looks into our Mysql DB to see if the email from there matches the one a User may have tried to register with \\
+        private bool CustomersExists(string email)
+        {
+            return _context.customers.Any(e => e.email_of_company_contact == email);
+        }
+    }
+}
---
+++ Controllers/InterventionsController.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterventionsController : ControllerBase
+    {
+        private readonly RestAPIContext _context;
+
+        public InterventionsController(RestAPIContext context)
+        {
+            _context = context;
+        }
+
+        //---------------------------------------Selects All Interventions------------------------------------------\\
+
+        // GET: api/Interventions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Intervention>>> Getinterventions()
+        {
+            return await _context.interventions.ToListAsync();
+        }
+
+        //---------------------------------------Selects One Intervention---------------------------------------------\\
+
+        // GET: api/Interventions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Intervention>> GetIntervention(long id)
+        {
+            var intervention = await _context.interventions.FindAsync(id);
+
+            if (intervention == null)
+            {
+                return NotFound();
+            }
+
+            return intervention;
+        }
+
+
+        //---------------------------------------GET INTERVENTION---------------------------------------------\\
+
+        // GET: api/Interventions/pending
+        [HttpGet("pending")]
+        public async Task<ActionResult<List<Intervention>>> GetPendingIntervention()
+        {
+            var allInterventions = await _context.interventions.Where(l => l.start_of_intervention == null).ToListAsync();
+            var newInterventions = allInterventions.Where(e => e.status == "Pending").ToList();
+            return newInterventions;
+        }
+
+
+        //---------------------------------------PUT #1 INTERVENTION---------------------------------------------\\
+
+        // PUT: api/Interventions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}/Inprogress")]
+        public async Task<IActionResult> PutIntervention(long id, Intervention intervention)
+        {
+            if (id != intervention.id)
+            {
+                return BadRequest();
+            }
+
+            Intervention interventionFound = await _context.interventions.FindAsync(id);
+            interventionFound.status = intervention.status;
+            interventionFound.start_of_intervention = DateTime.Now;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InterventionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //---------------------------------------PUT #2 INTERVENTION---------------------------------------------\\
+
+        // PUT: api/Interventions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+      [HttpPut("{id}/completed")]
+        public async Task<IActionResult> PutIntervention2(long id, Intervention intervention)
+        {
+            if (id != intervention.id)
+            {
+                return BadRequest();
+            }
+
+            Intervention interventionFound = await _context.interventions.FindAsync(id);
+            interventionFound.status = intervention.status;
+            interventionFound.end_of_intervention = DateTime.Now;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InterventionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        // POST: api/Interventions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPost]
+        // public async Task<ActionResult<Intervention>> PostIntervention(Intervention intervention)
+        // {
+        //     _context.interventions.Add(intervention);
+        //     await _context.SaveChangesAsync();
+
+        //     return CreatedAtAction("GetIntervention", new { id = intervention.id }, intervention);
+        // }
+
+        // DELETE: api/Interventions/5
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeleteIntervention(long id)
+        // {
+        //     var intervention = await _context.interventions.FindAsync(id);
+        //     if (intervention == null)
+        //     {
+        //         return NotFound();
+        //     }
+
+        //     _context.interventions.Remove(intervention);
+        //     await _context.SaveChangesAsync();
+
+        //     return NoContent();
+        // }
+
+        private bool InterventionExists(long id)
+        {
+            return _context.interventions.Any(e => e.id == id);
+        }
+    }
+    
+}
